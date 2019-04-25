db720a2542ae7eddb25f8916e1fdf63b49d0f8b2
organized-counter-5921 <kalapacs@gmail.com>
2019-04-25 21:50:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@ const playlist = {
   Pokolgep: "Farkasok dala"
 }
 
-function updatePlaylist(){
-
+function updatePlaylist(playlist, artist, song){
+  playlist[artist] = song;
+  return playlist;
 }
