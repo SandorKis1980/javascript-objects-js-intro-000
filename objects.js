8dfad29b4e5fcd55155d5dade97fbb043b295315
organized-counter-5921 <kalapacs@gmail.com>
2019-04-25 22:01:22 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist, artist, song){
   return playlist;
 }
 
-function removeFromPlaylist(playlist, playlist.artist){
+function removeFromPlaylist(playlist, artist){
   delete playlist.artist;
   return playlist;
 }
